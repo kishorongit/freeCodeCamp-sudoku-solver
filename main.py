c5a8924df6bca9ab80fb5ee82422ebf2ec10455d
Kishor Subedi <subedi.kishor1987@gmail.com>
2024-08-12 14:00:47 +0545

    feat: add functionality to find empty cell

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 class Board:
     def __init__(self, board):
         self.board = board
+
+    def find_empty_cell(self):
+        for row, contents in enumerate(self.board):
+            try:
+                col = contents.index(0)
+                return row, col
+            except ValueError:
+                pass
+        return None
