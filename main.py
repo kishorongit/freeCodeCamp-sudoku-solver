119c983d9f7df91d4d79f8a847dd8a77bbd97bf7
Kishor Subedi <subedi.kishor1987@gmail.com>
2024-08-12 15:16:46 +0545

    feat: add functionality to solve a given board

--- main.py
+++ main.py
@@ -32,3 +32,15 @@ class Board:
         valid_in_col = self.valid_in_col(col, num)
         valid_in_square = self.valid_in_square(row, col, num)
         return all([valid_in_row, valid_in_col, valid_in_square])
+
+    def solver(self):
+        if (next_empty := self.find_empty_cell()) is None:
+            return True
+        for guess in range(1, 10):
+            if self.is_valid(next_empty, guess):
+                row, col = next_empty
+                self.board[row][col] = guess
+                if self.solver():
+                    return True
+                self.board[row][col] = 0
+        return False
